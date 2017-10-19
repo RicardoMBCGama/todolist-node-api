dac6757276b3320b68055f823583de4b1955dce8
RicardoMBCGama <ricardombgama@gmail.com>
2017-10-19 17:48:04 +0100

    Set mongoose option 'useMongoClient' = true
    This option is required since mongoose>=4.11.0 when using connect().

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 var express = require("express")
-var app = express()
+var app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
 var Task = require("./api/models/todoListModel");
 var bodyParser = require("body-parser");
 
+var options = {
+  useMongoClient: true
+}
+
 //Database connection
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/Tododb");
+mongoose.connect("mongodb://localhost/Tododb",options);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var routes = require("./api/routes/todoListRoutes")
-routes(app;)
-//
-// app.get("/", (req,res) => {
-//   res.send("Hello World!");
-// });
+var routes = require("./api/routes/todoListRoutes");
+routes(app);
+
+app.get("/", (req,res) => {
+  res.send("Hello World!");
+});
 
 
 app.listen(port, () => {
